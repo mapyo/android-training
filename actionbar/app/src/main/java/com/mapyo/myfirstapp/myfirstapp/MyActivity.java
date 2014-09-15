cee1d6d9f408c3fb5dd059d2aab98fd74622c346
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-09-16 06:31:38 +0900

    finish Respond to Action Buttons

--- actionbar/app/src/main/java/com/mapyo/myfirstapp/myfirstapp/MyActivity.java
+++ actionbar/app/src/main/java/com/mapyo/myfirstapp/myfirstapp/MyActivity.java
@@ -41,11 +41,16 @@ public class MyActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 
     /**
