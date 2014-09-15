759d673e7e49183c45370c37e46c053b32250d76
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-09-15 18:25:39 +0900

    finish Start the Second Activity

--- MyFirstApp/app/src/main/java/com/mapyo/myfirstapp/myfirstapp/MyActivity.java
+++ MyFirstApp/app/src/main/java/com/mapyo/myfirstapp/myfirstapp/MyActivity.java
@@ -1,5 +1,6 @@
 package com.mapyo.myfirstapp.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,10 +11,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-
+import android.widget.EditText;
 
 
 public class MyActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "com.mapyo.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +66,10 @@ public class MyActivity extends ActionBarActivity {
 
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-        
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivities(intent);
     }
 }
