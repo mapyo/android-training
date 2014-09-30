0a77beded38da6040eba22cd6c5a8873a4b39bfe
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-09-30 09:58:31 +0900

    Create a New Instanceのようなものを少しやった

--- ActivityLifecycle/app/src/main/java/com/mapyo/activitylifecycle/MyActivity.java
+++ ActivityLifecycle/app/src/main/java/com/mapyo/activitylifecycle/MyActivity.java
@@ -4,14 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MyActivity extends Activity {
+
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        mTextView = (TextView)findViewById(R.id.text_message);
+        mTextView.setText("hogehoge");
+
     }
 
 
